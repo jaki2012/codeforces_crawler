b79a10c44a15a61520c20cf22e4ceece1df7c5fc
jaki2012 <jaki2012@sina.cn>
2018-11-30 18:42:32 +0800

    add a tool to help restore sourcefiles for corresponding source code

---
+++ gen_source_file.py
@@ -0,0 +1,59 @@
+from ast import literal_eval
+import os
+import sys
+
+def create_directory(d):
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+
+EXTENTIONS_DICT = {
+    'GNU C'        : 'c'       ,
+    'GNU C11'      : 'c'       ,
+    'GNU C++'      : 'cc'      ,
+    'GNU C++11'    : 'cc'      ,
+    'GNU C++0x'    : 'cc'      ,
+    'MS C++'       : 'cpp'     ,
+    'FPC'          : 'pp'      ,
+    'Delphi'       : 'pas'     ,
+    'Haskell'      : 'hs'      ,
+    'Java 8'       : 'java'    ,
+    'Java 7'       : 'java'    ,
+    'Java 6'       : 'java'    ,
+    'Python 3'     : 'py'      ,
+    'Python 2'     : 'py'      
+}
+
+if len(sys.argv) < 3:
+    print("Error, please check your parameters, and" + 
+            "explicitly specify the corresponding paths")
+    print("Usage: ")
+    print("=======================")
+    print("python gen_source_file.py <your_submissions_file> <output_dir_name>")
+    print("e.g. python gen_source_file.py submissions-20181130.json source code")
+
+
+json_file_path = sys.argv[1]
+output_dir_name = sys.argv[2]
+
+with open(json_file_path, 'r') as f:
+    submissions = literal_eval(f.read())
+
+create_directory(output_dir_name)
+# s means submission
+for s in submissions:
+    # the key content - source code
+    source_code = s['source_code']
+    # the programming language
+    language = s['language']
+    # extention name 
+    ext = EXTENTIONS_DICT[language]
+
+    src_file_name = '{}_{}_{}.{}'.format(s['round_id'], s['problem_name'], s['submission_id'], ext)
+
+    print("Generating: %s" % src_file_name)
+    create_directory('./{}/{}/{}/{}'.format(output_dir_name, s['round_id'], s['problem_name'], s['language']))
+
+    with open('./{}/{}/{}/{}/{}'.format(output_dir_name, s['round_id'], s['problem_name'], s['language'],
+                                        src_file_name), 'w') as output:
+        output.write(source_code)
